45aa62b3d49f750c00642ae20eba5d0f800f8edb
Reshmapurni <reshmapurniakula@gmail.com>
2025-06-17 14:27:07 +0530

    Add files via upload

---
+++ Variables types/InstanceVariables.java
@@ -0,0 +1,9 @@
+class InstanceVariables
+{
+int a=10;
+public static void main(String[]args)
+{
+InstanceVariables iv=new InstanceVariables();
+System.out.println(iv.a);
+}
+}
---
+++ Variables types/LocalVariables.java
@@ -0,0 +1,9 @@
+class LocalVariables
+{
+ static int a=10;
+public static void main(String[]args)
+{
+int a=10;
+System.out.print(a);
+}
+}
